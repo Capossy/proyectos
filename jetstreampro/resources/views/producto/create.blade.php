81739e76eff982c7af8c976ba9e99ca280c246a9
Capossy <118070471+Capossy@users.noreply.github.com>
2022-12-16 22:32:43 +0100

    jetstream logging crud

---
+++ jetstreampro/resources/views/producto/create.blade.php
@@ -0,0 +1,24 @@
+
+@extends('adminlte::page')
+
+@section('title', 'Creacion')
+
+@section('content_header')
+    <h1>Creacion de Producto</h1>
+@stop
+
+@section('content')
+<form action={{url('/productos')}} method="POST" enctype="multipart/form-data">
+    @csrf
+    @include('producto.form')
+
+</form>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+ 
+@stop
